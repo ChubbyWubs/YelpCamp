4bdade3acd9acf13ede3be29d82935c08873fbd5
Michael Zakhary <112494653+PheoCodeoCytoma@users.noreply.github.com>
2024-06-19 11:29:01 -0400

    firstCommit

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 
 app.get('/', (req, res) => {
-    res.send("Hello from YelpCamp!");
+    res.render('home');
 })
+
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", {campgrounds});
+})
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", {campground});
+})
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds/new", async (req, res) => {
+    const campground = new Campground({title: `{}`, location: `{}`});
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
